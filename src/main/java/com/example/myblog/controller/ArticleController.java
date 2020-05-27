ac6d66496a28824abd5f5872ea54a8992a120720
sehongpark <sehong.park86@gmail.com>
2020-05-28 00:27:14 +0900

    Create new article form.

--- src/main/java/com/example/myblog/controller/ArticleController.java
+++ src/main/java/com/example/myblog/controller/ArticleController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller // 컨트롤러 선언! 요청과 응답을 처리!
 public class ArticleController {
+
     @GetMapping("/articles") // 해당 요청을 처리하도록, 메소드를 등록!
     public String index(Model model) { // 모델 객체를 가져옴!
         // 데이터를 모델에 등록! 등록된 데이터는 뷰 페이지에서 사용 가능!
@@ -13,5 +14,10 @@ public class ArticleController {
         // value(내용): "안녕하세요, 반갑습니다!"
         model.addAttribute("msg", "안녕하세요, 반갑습니다!");
         return "articles/index"; // 뷰페이지 설정
+    }
+
+    @GetMapping("/articles/new") // GET 요청이 해당 url로 오면, 아래 메소드를 수행!
+    public String newArticle() {
+        return "articles/new"; // 보여줄 뷰 페이지
     }
 }
