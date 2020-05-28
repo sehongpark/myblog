6c02a6fdc1a0754a7f3ec2371069e741ed007fa1
sehongpark <sehong.park86@gmail.com>
2020-05-28 15:58:11 +0900

    Receive form data with DTO.

--- src/main/java/com/example/myblog/controller/ArticleController.java
+++ src/main/java/com/example/myblog/controller/ArticleController.java
@@ -1,9 +1,13 @@
 package com.example.myblog.controller;
 
+import com.example.myblog.dto.ArticleForm;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j // 로깅(logging) 기능 추가! Lombok 플러그인 설치 필요!
 @Controller // 컨트롤러 선언! 요청과 응답을 처리!
 public class ArticleController {
 
@@ -19,5 +23,11 @@ public class ArticleController {
     @GetMapping("/articles/new") // GET 요청이 해당 url로 오면, 아래 메소드를 수행!
     public String newArticle() {
         return "articles/new"; // 보여줄 뷰 페이지
+    }
+
+    @PostMapping("/articles") // Post 방식으로 "/articles" 요청이 들어오면, 아래 메소드 수행!
+    public String create(ArticleForm form) { // 폼 태그의 데이터가 ArticleForm 객체로 만들어 짐!
+        log.info(form.toString()); // ArticleForm 객체 정보를 확인!
+        return "redirect:/articles"; // 브라우저를 "/articles" url로 보냄!
     }
 }
---
+++ src/main/java/com/example/myblog/dto/ArticleForm.java
@@ -0,0 +1,35 @@
+package com.example.myblog.dto;
+
+public class ArticleForm {
+    private String title;
+    private String content;
+
+    public ArticleForm(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "ArticleForm{" +
+                "title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
