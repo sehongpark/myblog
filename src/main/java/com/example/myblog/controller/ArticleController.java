82dcffe0dfe874e7d627c18221aa611054fc8fc5
sehongpark <sehong.park86@gmail.com>
2020-05-29 17:23:48 +0900

    11: Show articles index.

--- src/main/java/com/example/myblog/controller/ArticleController.java
+++ src/main/java/com/example/myblog/controller/ArticleController.java
@@ -1,23 +1,36 @@
 package com.example.myblog.controller;
 
 import com.example.myblog.dto.ArticleForm;
+import com.example.myblog.entity.Article;
+import com.example.myblog.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Slf4j // 로깅(logging) 기능 추가! Lombok 플러그인 설치 필요!
+@RequiredArgsConstructor // final 필드 값을 알아서 가져옴! (@autowired 대체!)
 @Controller // 컨트롤러 선언! 요청과 응답을 처리!
 public class ArticleController {
 
+    // 리파지터리 객체 자동 삽입 됨! 위에서 @RequiredArgsConstructor 했음!
+    private final ArticleRepository articleRepository;
+
     @GetMapping("/articles") // 해당 요청을 처리하도록, 메소드를 등록!
-    public String index(Model model) { // 모델 객체를 가져옴!
-        // 데이터를 모델에 등록! 등록된 데이터는 뷰 페이지에서 사용 가능!
-        // key(변수명): "msg",
-        // value(내용): "안녕하세요, 반갑습니다!"
-        model.addAttribute("msg", "안녕하세요, 반갑습니다!");
-        return "articles/index"; // 뷰페이지 설정
+    public String index(Model model) { // 뷰 페이지로 데이터 전달을 위한 Model 객체 자동 삽입 됨!
+        // 모든 Article을 가져옴
+        // Iterable 인터페이스는 ArrayList의 부모 인터페이스
+        Iterable<Article> articleList = articleRepository.findAll();
+
+        // 뷰 페이지로 articleList 전달!
+        model.addAttribute("articles", articleList);
+
+        // 뷰 페이지 설정
+        return "articles/index";
     }
 
     @GetMapping("/articles/new") // GET 요청이 해당 url로 오면, 아래 메소드를 수행!
--- src/main/java/com/example/myblog/controller/HelloController.java
+++ src/main/java/com/example/myblog/controller/HelloController.java
@@ -1,13 +1,42 @@
 package com.example.myblog.controller;
 
+import com.example.myblog.entity.Article;
+import com.example.myblog.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller // 해당 클래스를, 컨트롤러로 선언!
 public class HelloController {
+
+    private final ArticleRepository articleRepository;
+
     @GetMapping("/") // 다음 url 요청을 받음 -> "/"를 보여줘 -> 아래 메소드를 수행!
     public String hello() {
         return "helloworld"; // helloworld.mustache 페이지를 반환!
     }
+
+    @GetMapping("/init") // GET 요청이 해당 url로 오면, 아래 메소드를 수행!
+    public String newArticle() {
+        // Article 저장용 객체 생성
+        List<Article> articleList = new ArrayList<>();
+
+        // Article 생성..!
+        for (int i = 0; i < 3; i++) {
+            String title = "제목-" + (char)('A' + i);
+            String content = "내용..." + i;
+            articleList.add(new Article(null, title, content));
+        }
+
+        // Article 저장
+        articleRepository.saveAll(articleList);
+
+        return "redirect:/articles"; // 보여줄 뷰 페이지
+    }
+
 }
