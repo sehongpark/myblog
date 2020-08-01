f2ddfd502f4303c927f22e4027144943b38f82f3
sehongpark <sehong.park86@gmail.com>
2020-08-02 03:02:10 +0900

    25: Refactor with service layer.

--- src/main/java/com/example/myblog/api/CommentApiController.java
+++ src/main/java/com/example/myblog/api/CommentApiController.java
@@ -5,6 +5,7 @@ import com.example.myblog.entity.Article;
 import com.example.myblog.entity.Comment;
 import com.example.myblog.repository.ArticleRepository;
 import com.example.myblog.repository.CommentRepository;
+import com.example.myblog.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,28 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CommentApiController {
 
-    private final CommentRepository commentRepository;
-    private final ArticleRepository articleRepository; // 게시글 리파지터리
-
+    private final CommentService commentService; // 서비스 레이어 객체
 
     @PostMapping("/api/comments/{articleId}")
     public Long create(@PathVariable Long articleId,
                        @RequestBody CommentForm form) {
-        // 댓글 확인
-        log.info("form: " + form.toString());
-        Comment comment = form.toEntity();
-        log.info("comment: " + form.toString());
-
-        // 댓글이 달릴 게시글을 가져옴!
-        Article article = articleRepository.findById(articleId)
-                .orElseThrow(
-                        () -> new IllegalArgumentException("댓글을 작성할 Article이 없습니다.")
-                );
-
-        // 댓글 엔티티에 게시글 엔티티를 등록
-        comment.stickTo(article);
-        log.info("written: " + comment.toString());
-        Comment saved = commentRepository.save(comment);
+        // 서비스 객체가 댓글 생성
+        Comment saved = commentService.create(articleId, form);
         log.info("saved: " + saved.toString());
         return saved.getId();
     }
---
+++ src/main/java/com/example/myblog/service/CommentService.java
@@ -0,0 +1,38 @@
+package com.example.myblog.service;
+
+import com.example.myblog.dto.CommentForm;
+import com.example.myblog.entity.Article;
+import com.example.myblog.entity.Comment;
+import com.example.myblog.repository.ArticleRepository;
+import com.example.myblog.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class CommentService {
+    private final ArticleRepository articleRepository;
+    private final CommentRepository commentRepository;
+
+    @Transactional
+    public Comment create(Long articleId, CommentForm form) {
+        // 폼 데이터를 엔티티 객체로 변경
+        log.info("form: " + form.toString());
+        Comment comment = form.toEntity();
+        log.info("comment: " + comment.toString());
+
+        // 댓글이 달릴 게시글을 가져옴!
+        Article article = articleRepository.findById(articleId)
+                .orElseThrow(
+                        () -> new IllegalArgumentException("댓글을 작성할 Article이 없습니다.")
+                );
+
+        // 댓글 엔티티에 게시글 엔티티를 등록
+        comment.stickTo(article);
+        log.info("written: " + comment.toString());
+        return commentRepository.save(comment);
+    }
+}
